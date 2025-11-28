7cc955ba672641c5fa6f5b7f0e537d9630957bf7
Калоян Димитров <136224811+LuckyKaloyan@users.noreply.github.com>
2025-11-28 21:26:15 +0200

    change

--- softuniReactPractice/src/App.jsx
+++ softuniReactPractice/src/App.jsx
@@ -3,12 +3,14 @@ import Body from './Body';
 import Header from './Header';
 import Footer from './Footer';
 import Typer from './Typer';
+import Timer from './Timer';
 
 export default function App() {
   return (
     <main>
    <Header />
    <Body />
+   <Timer />
    <Typer />
    <Footer />
    </main>
